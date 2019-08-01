0d533a36a4ec6b3751606715059bf7af5ae371d7
Lin Wei Tatt <weitatt@Lins-MacBook-Pro.local>
2019-08-01 14:44:05 +0800

    added google vision api

--- sugartracker_web/blueprints/foods/views.py
+++ sugartracker_web/blueprints/foods/views.py
@@ -1,21 +1,6 @@
 from flask import Flask, Blueprint, render_template, request, redirect
 from models.user import User
 
-@app.before_request
-def before_request():
-    db.connect()
-
-
-@app.after_request
-def after_request(response):
-    db.close()
-    return response
-
-
-@app.cli.command()
-def migrate():
-    db.evolve(ignore_tables={'base_model'})
-
 foods_blueprint = Blueprint('foods',
                             __name__,
                             template_folder='templates')
@@ -27,4 +12,4 @@ def new():
 
 @foods_blueprint.route('/<item_name>', methods=["GET"])
 def search(item_name):
-    food = 
+    pass
