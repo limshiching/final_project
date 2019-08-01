5ad404b0774e716970f125ae3c4676336829736c
Lim Shi Ching <limshiching@Lims-MacBook-Pro.local>
2019-08-01 14:29:25 +0800

    installed pip requests

--- sugartracker_api/blueprints/foods/views.py
+++ sugartracker_api/blueprints/foods/views.py
@@ -1,20 +1,31 @@
 from flask import Blueprint, jsonify, make_response
+import requests
 
 
 foods_api_blueprint = Blueprint('foods_api',
                              __name__,
                              template_folder='templates')
 
+
 @foods_api_blueprint.route('/', methods=['GET'])
 def index():
     food = Food.select()
     food_list = []
-    for food in foods
-    food_list.append({
-        'id' : food.item_id
-        'brand' : food.brand_name
-        'calories' : food.calories
-        'unit' : food.calories_unit
-        'sugar' : food.sugars
-    })
+    for food in foods:
+        food_list.append({
+            'id' : food.item_id,
+            'brand' : food.brand_name,
+            'calories' : food.calories,
+            'unit' : food.calories_unit,
+            'sugar' : food.sugars
+        })
+    response = {'data': food_list}
+    return make_response(jsonify(response), 200)
+
+@foods_api_blueprint('/<item_name>/', methods=['GET'])
+def search():
+
+
+
+
 
