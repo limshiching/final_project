3b94a57a36bca2d262a792f039a2e1d8260661e3
ylootsma <ylootsma@gmail.com>
2019-08-01 16:13:41 +0800

    'fix'

--- sugartracker_web/blueprints/users/views.py
+++ sugartracker_web/blueprints/users/views.py
@@ -1,4 +1,12 @@
 from flask import Blueprint, render_template
+from flask import Flask, Blueprint, render_template, request, flash, redirect, url_for, request
+from werkzeug.security import generate_password_hash
+from werkzeug.utils import secure_filename
+from models.user import User
+from flask_login import login_required, current_user
+import app
+import os
+
 
 
 users_blueprint = Blueprint('users',
@@ -11,9 +19,22 @@ def new():
     return render_template('users/new.html')
 
 
-@users_blueprint.route('/', methods=['POST'])
+@users_blueprint.route('/create', methods=['POST'])
 def create():
-    pass
+    name = request.form.get("fullname")
+    email = request.form.get('email')
+    password = request.form.get('pwd')
+    gender = request.form.get('gender')
+    length = request.form.get('length')
+    DOB = request.form.get('date')
+   
+    hashed_password = generate_password_hash(password)
+    user = User(name=name, email=email,
+                password=hashed_password, gender=gender, length=length, DOB=DOB)
+    if user.save():
+        return redirect(url_for('home'))
+    else:
+        return redirect(url_for('users.new'))
 
 
 @users_blueprint.route('/<username>', methods=["GET"])
