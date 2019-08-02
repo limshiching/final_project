1b62467d7c5102ebe57ea9af02a37c8a4018db0f
Lin Wei Tatt <weitatt@Lins-MacBook-Pro.local>
2019-08-02 18:11:35 +0800

    added clarifai key

--- sugartracker_web/blueprints/images/views.py
+++ sugartracker_web/blueprints/images/views.py
@@ -1,5 +1,8 @@
-from flask import redirect, Blueprint, render_template
+from flask import redirect, Blueprint, render_template, request, flash, url_for
 from clarifai.rest import ClarifaiApp
+from werkzeug.utils import secure_filename
+import os
+
 
 images_blueprint = Blueprint('images',
     __name__, template_folder='templates')
@@ -11,20 +14,17 @@ def new():
 
 @images_blueprint.route('/upload', methods=['POST'])
 def upload():
-
-    file = request.files['user_file']
-    if 'file' not in request.files:
-        return "No file"
-
-    image = request.form.get('image_name')
-
-    app = ClarifaiApp(api_key='5b3b1ba9fc7546b591655e33f31052af')
-    model = app.public_models.general_model
-    response = model.predict_by_url(image)
-
-    if response.save():
-        flash('Upload Successful')
-        return redirect(url_for('image.new'))
+    file = request.files.get('user_file')
+    breakpoint()
+    # url_input = request.params['url_input']
     
-    else:
-        return redirect(url_for('images.new'))
+    app = ClarifaiApp(api_key=os.getenv('CLARIFAI_KEY'))
+
+    # if not file.filename:
+        # return 'Please select a file'
+    # print(file.filename)
+    model = app.public_models.food_model
+    response = model.predict_by_url(url='https://samples.clarifai.com/puppy.jpeg')
+    # app.inputs.create_image_from_url(url='https://samples.clarifai.com/puppy.jpeg')
+    results = (response['outputs'][0]['data']['concepts'])
+    return render_template('images/new.html', results=results, response=response)
