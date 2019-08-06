80f80e1ac733985691824a900dae268e1dc1e521
Lin Wei Tatt <weitatt@Lins-MacBook-Pro.local>
2019-08-06 15:29:05 +0800

    added json

--- start.py
+++ start.py
@@ -1,9 +1,8 @@
 from app import app
 import sugartracker_api
 import sugartracker_web
-from flask_wtf.csrf import CSRFProtect
+
 
 if __name__ == '__main__':
     app.run()
 
-csrf=CSRFProtect(app)    
